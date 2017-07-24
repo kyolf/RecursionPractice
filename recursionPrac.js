b00679c08bc70e71a62835dee171be91ddb825f3
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-07-24 09:48:01 -0700

    finish problem 3 and 4

--- recursionPrac.js
+++ recursionPrac.js
@@ -33,3 +33,45 @@
 // }
 //
 // console.log(arrayDouble([5,8,10]));
+
+
+//Problem 3
+//input - string
+//output - reversed string
+//recursive input - string excluding the last char
+//recursive output - last char + entire string excluding last char
+
+// function reverseString(str){
+//   if(!str.length)
+//     return '';
+//   return str.charAt(str.length-1) + reverseString(str.slice(0,str.length-1));
+// }
+
+//input - string
+//output - reversed string
+//recursive input - string excluding the first char
+//recursive output - entire string excluding last char + first char
+
+// function reverseString(str){
+//   if(!str.length)
+//     return '';
+//   return reverseString(str.slice(1)) + str.charAt(0);
+// }
+
+// reverseString('slip');
+
+//Problem 4
+//input - nth number
+//output - nth triangular number
+//recursive input - nth number - 1
+//recursive output - nth number adding sum of the next couple nth number-1 until it reaches 1
+
+// function nthTriangular(num){
+//   if(num === 1)
+//     return 1;
+//   return num + nthTriangular(num-1);
+// }
+
+// nthTriangular(9);
+
+//Problem 5
