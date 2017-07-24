68c44a6a2c4b2bc0bdb1d5c707813d0e49591b85
patricewhite <patricewhite2012@gmail.com>
2017-07-24 12:10:08 -0400

    first 2

--- recursionPrac.js
+++ recursionPrac.js
@@ -1,1 +1,35 @@
 //Problem 1
+
+//input - number of sheep you have
+//output - message with number
+//input of recursive - number of sheep you have minus 1
+//output - total number of sheep that have jumped
+
+
+// function countingSheep(num){
+//   if( num === 0)
+//     return '';
+//   else{
+//     console.log(num,'Another sheep jumps over the fence');
+//     return countingSheep(num - 1);
+//   }
+// }
+
+//countingSheep(5);
+
+//Problem 2
+//input - arry of numbers
+//output - array of numbers with doubled value
+//recursive input - array of elments minus first element
+//recursive output - array first element doubled
+
+// function arrayDouble(arr){
+//   if (!arr.length)
+//     return [];
+//   else {
+//     let newArr = arr[0] * 2;
+//     return [newArr, ...arrayDouble(arr.slice(1))];
+//   }
+// }
+//
+// console.log(arrayDouble([5,8,10]));
